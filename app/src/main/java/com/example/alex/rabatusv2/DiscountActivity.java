3419eab80889986212ea5be00a0ff4189db69c97
Paaff <pafischer1992@gmail.com>
2015-06-18 12:23:13 +0200

    Added DiscountActivity

--- app/src/main/java/com/example/alex/rabatusv2/DiscountActivity.java
+++ app/src/main/java/com/example/alex/rabatusv2/DiscountActivity.java
@@ -1,7 +1,28 @@
 package com.example.alex.rabatusv2;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
 /**
  * Created by Mathias Linde on 18-06-2015.
  */
-public class DiscountActivity {
+public class DiscountActivity extends Activity {
+
+    //The main view.
+    private LinearLayout mFrame;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.discount_activity);
+
+        //Set the layout.
+        mFrame = (LinearLayout) findViewById(R.id.Discount_Main_Layout);
+
+
+    }
 }
