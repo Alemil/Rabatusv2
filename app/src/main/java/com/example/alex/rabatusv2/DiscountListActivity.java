ffec8e18e0b19fae869ee3c46c1885462169b051
lindebros <math.linde1994@gmail.com>
2015-06-22 10:23:16 +0200

    Something is happening... Bugged Highscore is evolving... It's becoming... Finished highscore!!!

--- app/src/main/java/com/example/alex/rabatusv2/DiscountListActivity.java
+++ app/src/main/java/com/example/alex/rabatusv2/DiscountListActivity.java
@@ -43,7 +43,7 @@ public class DiscountListActivity extends ListActivity {
 
 
         // Get the saved DiscountItems
-        Gson gson = new Gson();
+         Gson gson = new Gson();
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         //Restore the saved data using the DISCOUNT_PREF_KEY.
